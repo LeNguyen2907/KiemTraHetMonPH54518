e6b6a1c626c39d7284302b03da594f96f9868c3e
lenguyen2907 <lcinuen29@gmail.com>
2025-03-02 09:27:41 +0700

    pass

--- src/test/java/service/TestSelenium.java
+++ src/test/java/service/TestSelenium.java
@@ -1,4 +1,23 @@
-package service;
-
-public class TestSelenium {
-}
+//package service;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class TestSelenium {
+//
+//    ChromeDriver driver;
+//
+//    @BeforeEach
+//    public void setUp () {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//    }
+//
+//    @Test
+//    public void testSelenium () {
+//
+//        driver.get("https://www.zoho.com/vi/calendar/schedule-appointments.html");
+//    }
+//}
